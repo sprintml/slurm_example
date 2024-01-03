e17c4462f3bd3115b594696818773ee7a8076d1c
Adam Dziedzic <adam.dziedzi@gmail.com>
2024-01-03 22:33:55 +0100

    Update run-test.sh

--- run-test.sh
+++ run-test.sh
@@ -9,10 +9,13 @@
 
 # mitigates activation problems
 eval "$(conda shell.bash hook)"
+source .bashrc
 
 # activate the correct environment
 # conda activate pytorch
-source activate /sprint1/anaconda/envs/pytorch
+# source activate /sprint1/anaconda/envs/pytorch
+# source activate /home/${USER}/.conda/envs/test-env
+conda activate test-env
 
 # debug print-outs
 echo USER: $USER
